ac3ef4eeeca3c052b253331f646dcf552ebafb09
aff151 <aff151@inlumine.ual.es>
2018-11-10 11:42:14 +0100

    hecho diseño de crear proyecto

--- ProyectoNRP/src/clases/CrearProyecto.java
+++ ProyectoNRP/src/clases/CrearProyecto.java
@@ -1,6 +1,7 @@
 package clases;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
@@ -10,13 +11,16 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 public class CrearProyecto extends JFrame {
 
 	private JPanel contentPane;
-
+	private JTextField textField;
+	private JTextField textField_1;
 	/**
 	 * Launch the application.
 	 */
@@ -47,21 +51,44 @@ public class CrearProyecto extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnIrAMenu = new JButton("Ir a menu ");
-		btnIrAMenu.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Menu menu = new Menu();
-				menu.setVisible(true);
-				dispose();
-			}
-		});
-		btnIrAMenu.setBounds(204, 254, 107, 23);
-		contentPane.add(btnIrAMenu);
+		JButton btnAtras = new JButton("Atrás");
+		btnAtras.setBounds(473, 460, 117, 29);
+		contentPane.add(btnAtras);
 		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(Interfaz.class.getResource("/imagenes/fondo.png")));
-		lblNewLabel.setBounds(0, 0, 600, 500);
-		contentPane.add(lblNewLabel);
+		JButton btnCrearProyecto = new JButton("Crear Proyecto");
+		btnCrearProyecto.setBounds(256, 370, 133, 29);
+		contentPane.add(btnCrearProyecto);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(270, 18, 167, 26);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblIntrodProyecto = new JLabel("Introduce el nombre del nuevo proyecto");
+		lblIntrodProyecto.setBounds(35, 23, 246, 16);
+		contentPane.add(lblIntrodProyecto);
+		
+		JList list = new JList();
+		list.setBackground(Color.WHITE);
+		list.setBounds(35, 121, 189, 226);
+		contentPane.add(list);
+		
+		JLabel lblIntrodImport = new JLabel("Introduce la importancia del cliente para el proyecto");
+		lblIntrodImport.setBounds(256, 135, 334, 67);
+		contentPane.add(lblIntrodImport);
+		
+		textField = new JTextField();
+		textField.setBounds(342, 195, 80, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblSelecCliente = new JLabel("Seleccionar Cliente");
+		lblSelecCliente.setBounds(52, 93, 160, 16);
+		contentPane.add(lblSelecCliente);
+		
+		JLabel lblFondo = new JLabel("");
+		lblFondo.setIcon(new ImageIcon(CrearProyecto.class.getResource("/imagenes/fondo.png")));
+		lblFondo.setBounds(0, 0, 600, 500);
+		contentPane.add(lblFondo);
 	}
-
 }
