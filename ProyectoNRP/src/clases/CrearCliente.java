d4301d5a5a65b00d4cef48562f9ddd8a86818590
AlfonsoCabreraGimenez <acg125@inlumine.ual.es>
2018-11-10 13:27:38 +0100

    kkk

--- ProyectoNRP/src/clases/CrearCliente.java
+++ ProyectoNRP/src/clases/CrearCliente.java
@@ -2,12 +2,15 @@ package clases;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JPopupMenu;
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JMenuItem;
@@ -15,13 +18,14 @@ import javax.swing.JList;
 import javax.swing.JLabel;
 import java.awt.Color;
 import javax.swing.JTextField;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class CrearCliente extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField importanciaCliente;
+	private JTextField nombreTexttField;
 
 	/**
 	 * Launch the application.
@@ -43,6 +47,34 @@ public class CrearCliente extends JFrame {
 	 * Create the frame.
 	 */
 	public CrearCliente() {
+		
+		 setIconImage(Toolkit.getDefaultToolkit().getImage(Interfaz.class.getResource("/imagenes/icono.PNG")));
+		setResizable(false);
+		setBounds(100, 100, 600, 500);
+		setLocationRelativeTo(null);
+		setUndecorated(true);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnIrAMenu = new JButton("Ir a menu ");
+		btnIrAMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Menu menu = new Menu();
+				menu.setVisible(true);
+				dispose();
+			}
+		});
+		btnIrAMenu.setBounds(204, 254, 107, 23);
+		contentPane.add(btnIrAMenu);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(Interfaz.class.getResource("/imagenes/fondo.png")));
+		lblNewLabel.setBounds(0, 0, 600, 500);
+		contentPane.add(lblNewLabel);
+
+	//--------------------------------------------------------------------------------------------------------------------
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 767, 544);
 		contentPane = new JPanel();
@@ -55,35 +87,58 @@ public class CrearCliente extends JFrame {
 		list.setBounds(35, 121, 189, 226);
 		contentPane.add(list);
 		
-		JLabel lblNewLabel = new JLabel("Seleccionar Proyecto");
-		lblNewLabel.setBounds(52, 93, 160, 16);
-		contentPane.add(lblNewLabel);
+		JLabel lblNewLabel1 = new JLabel("Seleccionar Proyecto");
+		lblNewLabel1.setBounds(52, 93, 160, 16);
+		contentPane.add(lblNewLabel1);
 		
 		JLabel lblIntroduceElPeso = new JLabel("Introduce la importancia  al cliente para el proyecto");
 		lblIntroduceElPeso.setBounds(353, 131, 334, 67);
 		contentPane.add(lblIntroduceElPeso);
 		
-		textField = new JTextField();
-		textField.setBounds(438, 198, 130, 26);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		importanciaCliente = new JTextField();
+		importanciaCliente.setBounds(438, 198, 130, 26);
+		contentPane.add(importanciaCliente);
+		importanciaCliente.setColumns(10);
 		
-		JButton btnNewButton = new JButton("Crear Cliente");
-		btnNewButton.setBounds(633, 469, 117, 29);
-		contentPane.add(btnNewButton);
+		JButton crearClienteBoton = new JButton("Crear Cliente");
+		crearClienteBoton.setBounds(633, 469, 117, 29);
+		contentPane.add(crearClienteBoton);
+
+		/////////////////////////////////////
+		crearClienteBoton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//IR A LA BBDD PARA METER AL CLIENTE
+				String nombreCliente = nombreTexttField.getText();
+				
+				
+			}
+		});
 		
-		JButton btnNewButton_1 = new JButton("Atrás");
-		btnNewButton_1.setBounds(6, 469, 117, 29);
-		contentPane.add(btnNewButton_1);
+		//////////////////////////////
+
+		
+		
+		JButton volverInicio = new JButton("Atrás");
+		volverInicio.setBounds(6, 469, 117, 29);
+		contentPane.add(volverInicio);
+		
+		volverInicio.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Menu m = new Menu();
+				m.setVisible(true);
+				dispose();
+				
+			}
+		});
 		
 		JLabel lblIntroduceElNombre = new JLabel("Introduce el nombre del nuevo cliente");
 		lblIntroduceElNombre.setBounds(35, 23, 246, 16);
 		contentPane.add(lblIntroduceElNombre);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(332, 18, 320, 26);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		nombreTexttField = new JTextField();
+		nombreTexttField.setBounds(332, 18, 320, 26);
+		contentPane.add(nombreTexttField);
+		nombreTexttField.setColumns(10);
 	}
 
 	private static void addPopup(Component component, final JPopupMenu popup) {
@@ -103,4 +158,7 @@ public class CrearCliente extends JFrame {
 			}
 		});
 	}
+	
+
 }
+
--- ProyectoNRP/src/database/BD_Clientes.java
+++ ProyectoNRP/src/database/BD_Clientes.java
@@ -2,7 +2,17 @@ package database;
 
 import java.util.Vector;
 
+import org.orm.PersistentException;
+import org.orm.PersistentTransaction;
+
 public class BD_Clientes {
 	public BD_Principal _c_bd_clien;
 	public Vector<Cliente> _cont_clientes = new Vector<Cliente>();
+	
+	public void crearCliente (String nombreCliente, String nombreProyecto) throws PersistentException
+	{
+		PersistentTransaction t = database.BasededatosPersistentManager.instance().getSession().beginTransaction();
+		
+
+	}
 }
