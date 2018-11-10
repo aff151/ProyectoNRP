d350be374d03382f2c91452c8b3b95f2b9750732
jmr779 <jmr779@inlumine.ual.es>
2018-11-10 11:41:05 +0100

    subida de classpath

--- ProyectoNRP/src/clases/ModificarProyecto.java
+++ ProyectoNRP/src/clases/ModificarProyecto.java
@@ -39,7 +39,7 @@ public class ModificarProyecto extends JFrame {
 	 * Create the frame.
 	 */
 	public ModificarProyecto() {
-		
+
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Interfaz.class.getResource("/imagenes/icono.PNG")));
 		setResizable(false);
 		setBounds(100, 100, 600, 500);
@@ -49,42 +49,42 @@ public class ModificarProyecto extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnSalir.setBackground(new Color(204, 0, 0));
+		btnSalir.setForeground(Color.BLACK);
+		btnSalir.setBounds(487, 450, 89, 23);
+		contentPane.add(btnSalir);
 		
 		JLabel lblConsultaDelProyecto = new JLabel("Modificación del proyecto...");
 		lblConsultaDelProyecto.setBounds(31, 40, 184, 16);
 		contentPane.add(lblConsultaDelProyecto);
-		
-			JButton btnSalir = new JButton("Salir");
-			btnSalir.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					System.exit(0);
-				}
-			});
-			btnSalir.setBackground(new Color(204, 0, 0));
-			btnSalir.setForeground(Color.BLACK);
-			btnSalir.setBounds(487, 450, 89, 23);
-			contentPane.add(btnSalir);
-		
+
 		JList list = new JList();
 		list.setBounds(68, 154, 124, 199);
 		contentPane.add(list);
-		
+
 		JList list_1 = new JList();
 		list_1.setBounds(353, 154, 124, 199);
 		contentPane.add(list_1);
-		
+
 		JLabel lblListaDeClientes = new JLabel("Lista de clientes del proyecto");
 		lblListaDeClientes.setBounds(31, 126, 217, 16);
 		contentPane.add(lblListaDeClientes);
-		
+
 		JLabel lblListaDeRequisitos = new JLabel("Lista de requisitos del proyecto");
 		lblListaDeRequisitos.setBounds(313, 126, 217, 16);
 		contentPane.add(lblListaDeRequisitos);
-		
+
 		JButton btnAadirMsClientes = new JButton("Eliminar Clientes");
 		btnAadirMsClientes.setBounds(45, 365, 170, 29);
 		contentPane.add(btnAadirMsClientes);
-		
+
 		JButton btnAadirMsRequisitos = new JButton("Eliminar Requisitos");
 		btnAadirMsRequisitos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -92,12 +92,11 @@ public class ModificarProyecto extends JFrame {
 		});
 		btnAadirMsRequisitos.setBounds(333, 365, 170, 29);
 		contentPane.add(btnAadirMsRequisitos);
-		
+
 		JButton btnAtrs = new JButton("Atrás");
 		btnAtrs.setBounds(389, 447, 77, 29);
 		contentPane.add(btnAtrs);
-		
-		
+
 		JButton btnAadirMsClientes_1 = new JButton("Añadir más Clientes");
 		btnAadirMsClientes_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -105,14 +104,15 @@ public class ModificarProyecto extends JFrame {
 		});
 		btnAadirMsClientes_1.setBounds(45, 391, 170, 29);
 		contentPane.add(btnAadirMsClientes_1);
-		
+
 		JButton btnAadirMsRequisitos_1 = new JButton("Añadir más Requisitos");
 		btnAadirMsRequisitos_1.setBounds(333, 391, 170, 29);
 		contentPane.add(btnAadirMsRequisitos_1);
 		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(Interfaz.class.getResource("/imagenes/fondo.png")));
-		lblNewLabel.setBounds(0, 0, 600, 500);
-		contentPane.add(lblNewLabel);
+				JLabel lblNewLabel = new JLabel("");
+				lblNewLabel.setIcon(new ImageIcon(Interfaz.class.getResource("/imagenes/fondo.png")));
+				lblNewLabel.setBounds(0, 0, 600, 500);
+				contentPane.add(lblNewLabel);
+
 	}
 }
