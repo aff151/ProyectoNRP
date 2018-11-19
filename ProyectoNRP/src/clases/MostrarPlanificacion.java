c53085be32d7b6b1325d63e6724e1ca2a7b6da87
jmr779 <jmr779@inlumine.ual.es>
2018-11-19 13:13:44 +0100

    Nueva Clase!!!!!! Y mas trabajo!!!!!

---
+++ ProyectoNRP/src/clases/MostrarPlanificacion.java
@@ -0,0 +1,57 @@
+package clases;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class MostrarPlanificacion extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MostrarPlanificacion frame = new MostrarPlanificacion();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MostrarPlanificacion() {
+		inicializar();
+		
+	}
+
+	public void inicializar() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Menu.class.getResource("/imagenes/icono.PNG")));
+		setResizable(false);
+		setBounds(100, 100, 632, 427);
+		setLocationRelativeTo(null);
+		setTitle("Planificación del proyecto" + ConsultarProyectosPlanificacion.proySeleccionado);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblProyecto = new JLabel("Planificación del proyecto" + ConsultarProyectosPlanificacion.proySeleccionado);
+		lblProyecto.setBounds(20, 11, 332, 14);
+		contentPane.add(lblProyecto);
+		
+	}
+}
