0808b3a5e0628add8f8ce232a6c5f65d4af91613
jmr779 <jmr779@inlumine.ual.es>
2018-11-19 10:14:06 +0100

    Comienzo del guardar la matriz

--- ProyectoNRP/src/database/BD_Valor.java
+++ ProyectoNRP/src/database/BD_Valor.java
@@ -12,90 +12,92 @@ import database.Valor;
 public class BD_Valor {
 	public BD_Principal _c_bd_peso;
 	public Vector<Valor> _cont_peso = new Vector<Valor>();
-	
+
 	public List<Requisito> cargarRequisitosClienteProyecto(String proyecto, String cliente) throws PersistentException {
 		List<Requisito> listRequisitos = new ArrayList<Requisito>();
-		for(Valor v: ValorDAO.listValorByQuery(null, null)) {
-			if (v.getProyecto().getNombre().equals(proyecto) 
-					&& v.getCliente().getNombre().equals(cliente)) {
+		for (Valor v : ValorDAO.listValorByQuery(null, null)) {
+			if (v.getProyecto().getNombre().equals(proyecto) && v.getCliente().getNombre().equals(cliente)) {
 				listRequisitos.add(v.getRequisito());
 			}
 		}
 		return listRequisitos;
 	}
-	public List<Valor> cargarValorRequisitosClienteProyecto(String proyecto, String cliente) throws PersistentException {
+
+	public List<Valor> cargarValorRequisitosClienteProyecto(String proyecto, String cliente)
+			throws PersistentException {
 		List<Valor> listValor = new ArrayList<Valor>();
-		for(Valor v: ValorDAO.listValorByQuery(null, null)) {
-			if (v.getProyecto().getNombre().equals(proyecto) 
-					&& v.getCliente().getNombre().equals(cliente)) {
+		for (Valor v : ValorDAO.listValorByQuery(null, null)) {
+			if (v.getProyecto().getNombre().equals(proyecto) && v.getCliente().getNombre().equals(cliente)) {
 				listValor.add(v);
 			}
 		}
 		return listValor;
 	}
+
 	public List<Valor> cargarValorProyecto(String proyecto) throws PersistentException {
 		List<Valor> listValor = new ArrayList<Valor>();
-		for(Valor v: ValorDAO.listValorByQuery(null, null)) {
-			if (v.getProyecto().getNombre().equals(proyecto)) 
+		for (Valor v : ValorDAO.listValorByQuery(null, null)) {
+			if (v.getProyecto().getNombre().equals(proyecto))
 				listValor.add(v);
 		}
 		return listValor;
 	}
-	
-	public void crearValor(String pro,String cli,String req,String valor) throws PersistentException
-	{
 
+	public void crearValor(String pro, String cli, String req, String valor) throws PersistentException {
 
 		Valor v = ValorDAO.createValor();
 		Cliente client = null;
 		Proyecto proyec = null;
 		Requisito requi = null;
-		//coger Cliente
-		for(Cliente c : ClienteDAO.listClienteByQuery(null, null))
-		{
-			if(cli.equals(c.getNombre()))
+		// coger Cliente
+		for (Cliente c : ClienteDAO.listClienteByQuery(null, null)) {
+			if (cli.equals(c.getNombre()))
 				client = c;
 		}
-		//Cogemos proyecto
-		for(Proyecto p : ProyectoDAO.listProyectoByQuery(null, null))
-		{
-			if(pro.equals(p.getNombre()))
+		// Cogemos proyecto
+		for (Proyecto p : ProyectoDAO.listProyectoByQuery(null, null)) {
+			if (pro.equals(p.getNombre()))
 				proyec = p;
 		}
-		//Cogemos Requisito
-		for(Requisito r : RequisitoDAO.listRequisitoByQuery(null, null))
-		{
-			if(req.equals(r.getNombre()))
+		// Cogemos Requisito
+		for (Requisito r : RequisitoDAO.listRequisitoByQuery(null, null)) {
+			if (req.equals(r.getNombre()))
 				requi = r;
 		}
-		//Añadimos los valores al valor
+		// Añadimos los valores al valor
 		v.setCliente(client);
 		v.setProyecto(proyec);
 		v.setRequisito(requi);
 		v.setValor(Integer.parseInt(valor));
-		
+
 		ValorDAO.save(v);
-	
+
 	}
-	
-	public boolean modificarValor(String pro,String cli,String req,String valor) throws PersistentException
-	{
+
+	public boolean modificarValor(String pro, String cli, String req, String valor) throws PersistentException {
 		boolean existe = false;
 		Valor val = null;
-		for(Valor v : ValorDAO.listValorByQuery(null, null))
-		{
-			if(pro.equals(v.getProyecto().getNombre()) && cli.equals(v.getCliente().getNombre()) && req.equals(v.getRequisito().getNombre()))
-			{
+		for (Valor v : ValorDAO.listValorByQuery(null, null)) {
+			if (pro.equals(v.getProyecto().getNombre()) && cli.equals(v.getCliente().getNombre())
+					&& req.equals(v.getRequisito().getNombre())) {
 				val = v;
 				existe = true;
 			}
-				
+
 		}
-		if(existe == true)
-		{
+		if (existe == true) {
 			val.setValor(Integer.parseInt(valor));
 		}
-		
+
 		return (existe == true) ? true : false;
+	}
+
+	public boolean modificarMatrizValores(ArrayList<String> valoresMatriz, String proySeleccionado) throws PersistentException {
+		PersistentTransaction t = database.BasededatosPersistentManager.instance().getSession().beginTransaction();
+		Cliente cli = new Cliente();
+		Requisito req = new Requisito();
+		
+
+		return false;
 	}
 }
